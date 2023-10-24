1886412af56cae45437472c7ff3a0b536d2cfbd2
Jaegeuk Kim <jaegeuk@google.com>
2023-10-24 18:14:15 +0800

    clover: init: Use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Bug: 149391799
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I8cebe30bdf66f39f0e4cc5955e53aca4d4f22f64

--- device.mk
+++ device.mk
@@ -49,6 +49,12 @@ BOARD_HAVE_QCOM_FM := false
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# Partitions
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
