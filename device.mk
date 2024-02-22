a796b6f7135684df2cb5f0fcccb36a2f604ca5d9
Giovanni Ricca <giovanniricca@proton.me>
2024-02-23 01:13:24 +0800

    clover: device: Build fpc interface from source
    Change-Id: I80dd66139d1e53873beb14f7937cbfb721e582ca

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
     camera.sdm660 \
     libstdc++_vendor
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor
+
 # FM
 BOARD_HAVE_QCOM_FM := false
 
