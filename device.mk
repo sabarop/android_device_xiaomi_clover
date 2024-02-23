7662bd601f760e5711cc3b2ed22a42c8df61056d
Sabar <31942277+sabarop@users.noreply.github.com>
2024-02-23 14:26:38 +0700

    clover: commonize build some rules and modules

--- device.mk
+++ device.mk
@@ -7,12 +7,6 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
-# Project ID Quota
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # APEX
 ENABLE_APEX := true
 
@@ -29,8 +23,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml
 
 PRODUCT_PACKAGES += \
-    camera.sdm660 \
-    libstdc++_vendor
+    camera.sdm660
 
 # Fingerprint
 PRODUCT_PACKAGES += \
