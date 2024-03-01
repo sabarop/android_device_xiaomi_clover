c8c2e048cce33f4371fc67ab65285acfd7935581
Sabar <31942277+sabarop@users.noreply.github.com>
2024-03-01 12:48:27 +0700

    clover: add missing overlay-lineage

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ BOARD_HAVE_IR := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_PACKAGES += \
