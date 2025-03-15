6213607aa757016e6cd4bc899be9baa9ffd8dce8
Michael Bestas <1216752+mikeng@users.noreply.github.com>
2025-03-15 19:02:31 +0700

    clover: tools: Sync extract scripts with templates

--- extract-files.sh
+++ extract-files.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libmmcamera_bokeh.so)
+            [ "$2" = "" ] && return 0
+           "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libmmcamera_bokeh.so)
+            [ "$2" = "" ] && return 0
+           "${PATCHELF}" --remove-needed "libgui.so" "${2}"
+            ;;
+        vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so)
+            [ "$2" = "" ] && return 0
+           "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
+           "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
 
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
@@ -25,19 +40,9 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=clover
 export DEVICE_COMMON=sdm660-common
-export DEVICE_SPECIFIED_COMMON_DEVICE="clover"
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-export DEVICE_BRINGUP_YEAR=2018
-
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-patchelf --remove-needed libandroid.so "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera_bokeh.so
-patchelf --remove-needed libgui.so "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera_ppeiscore.so
-
-patchelf --remove-needed libhidltransport.so "$DEVICE_BLOB_ROOT"/vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so
-patchelf --replace-needed libhidlbase.so libhidlbase-v32.so "$DEVICE_BLOB_ROOT"/vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so
-
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
