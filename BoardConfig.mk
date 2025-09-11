3daf8c5b668c6b5e92837f8eaf7e4577d6e1323f
sabarop <sabar.pakpahan@gmail.com>
2025-09-11 20:41:45 +0700

    clover: Set VENDOR_SECURITY_PATCH to platform
    Set the VENDOR_SECURITY_PATCH level to match the platform
    security patch level. These are functionally the same.
    
    Co-Authored-By: Chris Fries <cfries@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-04-05
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
 
 # Inherit the proprietary files
 include vendor/xiaomi/clover/BoardConfigVendor.mk
