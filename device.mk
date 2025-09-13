e63440cb37a9442a383dd3bea807d40880ce341b
sabarop <sabar.pakpahan@gmail.com>
2025-09-13 16:01:32 +0700

    clover: commonize USB HAL

--- device.mk
+++ device.mk
@@ -87,10 +87,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
-
 # Wifi
 PRODUCT_PACKAGES += \
     CloverWifiOverlay
