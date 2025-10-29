c0783d2f464484883f494f9486c29a4dabf14192
sabarop <sabar.pakpahan@gmail.com>
2025-10-29 21:26:29 +0700

    clover: Build fingerprint HIDL

--- device.mk
+++ device.mk
@@ -36,6 +36,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sdm660 \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
     com.fingerprints.extension@1.0.vendor
 
 # Folio
